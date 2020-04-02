827c46796c59d40527ffac9a5941b5fd9182c3c1
Stephen <{11651034+stephenenright@users.noreply.github.com>
2020-04-02 16:49:40 -0600

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: "production",
     entry: "./src/index.js",
     output: {
-        path: path.resolve("build"),
+        path: path.resolve("lib"),
         filename: "index.js",
         libraryTarget: "commonjs2"
     },
